4b033c1759752f3191973d8bde0493124b3a66a9
biscake <leonut.ljh@gmail.com>
2024-04-05 16:02:24 +0800

    Attempt to fix live preview

--- javascript.js
+++ javascript.js
@@ -1,31 +1,77 @@
 const CHOICE = ["rock", "paper", "scissor"];
 const ROUNDS = 5;
 
-function getComputerChoice(choice) {
-    return choice[Math.floor(choice.length * Math.random())];
+function getComputerChoice() {
+    return CHOICE[Math.floor(CHOICE.length * Math.random())];
 }
 
-function playerSelection() {
-    let userInput;
-    do
-    {
-        userInput = prompt("Your Choice: ");
-    }
-    while (!CHOICE.includes(userInput.toLowerCase()))
-    return userInput.toLowerCase();
-}
 
-function playRound(getComputerChoice, playerSelection) {
-    if (getComputerChoice === playerSelection) {
+function playRound(computerChoice, playerChoice) {
+    if (computerChoice === playerChoice) {
         return "Draw!"
     }
-    else if ((getComputerChoice === "rock" && playerSelection === "scissor") ||
-    (getComputerChoice === "paper" && playerSelection === "rock") ||
-    (getComputerChoice === "scissor" && playerSelection === "paper")) {
-            return "You lose!";
+    else if ((computerChoice === "rock" && playerChoice === "scissor") ||
+    (computerChoice === "paper" && playerChoice === "rock") ||
+    (computerChoice === "scissor" && playerChoice === "paper")) {
+            return "You Lose!";
         }
     else {
         return "You Win!";
     }
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
+const playerImg = document.querySelector("#playerimg");
+const cpuImg = document.querySelector("#cpuimg");
+const scoreText = document.querySelector(".score");
+const outcomeText = document.querySelector(".outcome");
+
+const playerSelection = document.querySelector(".selection");
+playerSelection.addEventListener("click", (event) => {
+    let playerChoice = event.target.id;
+    const computerChoice = getComputerChoice();
+    const outcome = playRound(computerChoice, playerChoice);
+    updateOutcome(outcome);
+    updateScore();
+    updateImg(playerChoice, computerChoice);
+    if (playerScore === 5 || computerScore === 5) {
+        endGame();
+    }
+});
+
+function updateOutcome(outcome) {
+    outcomeText.textContent = outcome;
+    switch(outcome) {
+        case "You Win!":
+            playerScore++;
+            break;
+        case "You Lose!":
+            computerScore++;
+            break;
+    }
+}
+
+function updateScore() {
+    scoreText.textContent = `Your Score: ${playerScore}`+ "\u00A0\u00A0\u00A0" + `CPU Score: ${computerScore}`;
+}
+
+function updateImg(playerChoice, computerChoice) {
+    playerImg.src = `../img/${playerChoice}.svg`;
+    cpuImg.src = `../img/${computerChoice}.svg`;
+}
+
+
+function endGame() {
+    if (playerScore === 5) {
+        updateImg("winner", "loser");
+        outcomeText.textContent = "You're a winner!";
+    }
+    else {
+        updateImg("loser", "winner");
+        outcomeText.textContent = "Try again next time...";
+    }
+    playerScore = 0;
+    computerScore = 0;
+}
